1fd7a6c933abc10c2d2c808d8446cf61b8a86329
Simon Manton Milne <0005557m@student.gla.ac.uk>
2020-02-11 17:13:11 +0000

    Rango speaks for the first time

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rango import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
+    # The above maps any URLs starting with rango/ to be handled by rango.
     path('admin/', admin.site.urls),
 ]
